9d7415bb36bb2ed471a54ec0cd2e5d033cfd87c8
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-04-15 11:08:09 +0200

    deply all

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:pabman11/pabman11.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:pabman11/astro-pizza.git main:gh-pages
+git push -f git@github.com:pabman11/astro-pizza.git master:gh-pages
 
 cd -
