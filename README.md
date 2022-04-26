31578901b20b3b7a1b3a5eec7beb0db2c1cf0b45
Pablo Fornali <pablofornalimanzanas@gmail.com>
2022-04-26 19:08:58 +0200

    a

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Using and adapting icons from https://heroicons.com/
 
 Based on https://dribbble.com/shots/18017961-Food-delivery-app
 ![](https://cdn.dribbble.com/users/4137552/screenshots/18017961/media/faac3923ed3090e01d8c239e076d72c2.jpg)
-
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< Updated upstream
   integrations: [tailwind()]
 });
+=======
+  site: "https://pabman11.github.io/",
+  base: "/astro-pizza/",
+  integrations: [tailwind()],
+});
+>>>>>>> Stashed changes
